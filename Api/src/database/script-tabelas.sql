8e519b77cfe1da12fec37569ec4f6fa70c68b5a6
Fabricio-Nunes-Ernandes <fabricio.ernandes@sptech.school>
2024-11-12 14:19:56 -0300

    Conexão Api login e cadastro

---
+++ Api/src/database/script-tabelas.sql
@@ -0,0 +1,18 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql server
+*/
+
+create database EAFC;
+use EAFC;
+
+create table usuario(
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(60),
+telefone char(11),
+senha varchar(45)
+);
