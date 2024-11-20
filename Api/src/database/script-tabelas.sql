36278f7ae69ac881e77e4e17c82b143fdafa12ac
Fabricio-Nunes-Ernandes <fabricio.ernandes@sptech.school>
2024-11-20 05:06:38 -0300

    salvando questões no BD

--- Api/src/database/script-tabelas.sql
+++ Api/src/database/script-tabelas.sql
@@ -13,16 +13,16 @@ create table usuario(
 idUsuario int primary key auto_increment,
 nome varchar(45),
 email varchar(60),
-telefone char(11),
+celular char(11),
 senha varchar(45)
 );
 
 create table tentativa(
 idTentativa int primary key auto_increment,
 titulo varchar(60),
-resposta tinyint,
+resposta varchar(45),
 pontos int,
-genero varchar(8),
+genero varchar(9),
 fkUsuario int,
 constraint fkUsurioTentativa foreign key (fkUsuario) references usuario(idUsuario)
 );
